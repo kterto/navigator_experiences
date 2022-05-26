b23b1fafe961307e3964c1d5c2ba798f3bfb6711
Kainã Terto <kterto@capyba.com>
2022-05-26 13:30:48 -0300

    first interaction

---
+++ lib/src/book_app.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:navigator_experiences/src/models/book.dart';
+import 'package:navigator_experiences/src/pages/book_details_screen.dart';
+import 'package:navigator_experiences/src/pages/books_list_screen.dart';
+
+class BooksApp extends StatefulWidget {
+  const BooksApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _BooksAppState();
+}
+
+class _BooksAppState extends State<BooksApp> {
+  // New:
+  Book? _selectedBook;
+  bool show404 = false;
+  List<Book> books = [
+    Book('Left Hand of Darkness', 'Ursula K. Le Guin'),
+    Book('Too Like the Lightning', 'Ada Palmer'),
+    Book('Kindred', 'Octavia E. Butler'),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Books App',
+      home: Navigator(
+        pages: [
+          MaterialPage(
+            key: const ValueKey('BooksListPage'),
+            child: BooksListScreen(
+              books: books,
+              onTapped: _handleBookTapped,
+            ),
+          ),
+          if (_selectedBook != null)
+            MaterialPage(
+              key: ValueKey(_selectedBook),
+              child: BookDetailsScreen(
+                book: _selectedBook!,
+              ),
+            ),
+        ],
+        onPopPage: (route, result) {
+          if (!route.didPop(result)) {
+            return false;
+          }
+
+          // Update the list of pages by setting _selectedBook to null
+          setState(() {
+            _selectedBook = null;
+          });
+
+          return true;
+        },
+      ),
+    );
+  }
+
+  void _handleBookTapped(Book book) {
+    setState(() {
+      _selectedBook = book;
+    });
+  }
+}
