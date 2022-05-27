4545565f253c4088344e215850570e4de53336af
Kainã Terto <kterto@capyba.com>
2022-05-27 15:24:22 -0300

    initial_structure

---
+++ lib/posts/posts_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:navigator_experiences/data/app_data.dart';
+import 'package:navigator_experiences/widgets.dart';
+
+class PostsPage extends StatelessWidget {
+  PostsPage({Key? key}) : super(key: key);
+  final posts = Post.posts;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (int i = 0; i < posts.length; i++)
+              PostTile(
+                tileColor: posts[i].color,
+                postTitle: posts[i].title,
+                onTileTap: () {},
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
